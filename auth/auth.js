da7a58b384a1e3bce011d9342b6e488734ec1886
therahmonqulov <therahmonqulov@gmail.com>
2025-09-30 21:48:06 +0500

    add

--- auth/auth.js
+++ auth/auth.js
@@ -2,6 +2,8 @@ const loginForm = document.getElementById("login-form");
 const registerForm = document.getElementById("register-form");
 const showRegisterLink = document.getElementById("show-register");
 const showLoginLink = document.getElementById("show-login");
+const confirmPasswordInput = document.getElementById("confirm-password");
+
 
 showRegisterLink.addEventListener("click", (e) => {
     e.preventDefault();
@@ -14,3 +16,16 @@ showLoginLink.addEventListener("click", (e) => {
     registerForm.style.display = "none";
     loginForm.style.display = "block";
 });
+
+confirmPasswordInput.addEventListener("input", () => {
+    const password = document.getElementById("password").value;
+    const confirmPassword = confirmPasswordInput.value;
+    const errorSpan = document.querySelector(".register-confirm-password-error");
+
+    if (confirmPassword !== password) {
+        errorSpan.textContent = "Parollar mos kelmayapti";
+    } else {
+        errorSpan.textContent = "";
+    }
+});
+
