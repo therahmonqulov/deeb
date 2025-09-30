433c8282ff26b71812c3403c878a2515a2c0df69
therahmonqulov <therahmonqulov@gmail.com>
2025-09-30 21:48:09 +0500

    add

--- auth/auth.js
+++ auth/auth.js
@@ -17,12 +17,33 @@ showLoginLink.addEventListener("click", (e) => {
     loginForm.style.display = "block";
 });
 
-confirmPasswordInput.addEventListener("input", () => {
+registerForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+
     const password = document.getElementById("password").value;
     const confirmPassword = confirmPasswordInput.value;
     const errorSpan = document.querySelector(".register-confirm-password-error");
+    const emailErrorSpan = document.querySelector(".register-email-error");
+    const nameErrorSpan = document.querySelector(".register-name-error");
+    const registerPasswordErrorSpan = document.querySelector(".register-password-error");
+    const loginEmailErrorSpan = document.querySelector(".login-email-error");
+    const loginPasswordErrorSpan = document.querySelector(".login-password-error");
+    const emailInput = document.getElementById("email").value;
 
-    if (confirmPassword !== password) {
+    // parol uzunligini tekshirish
+    if (password.length <= 8 || password === "" ) {
+        registerPasswordErrorSpan.textContent = "Parol kamida 8 ta belgidan iborat bo'lishi kerak";
+    }
+
+    // email manzilining @gmail.com bilan tugashini tekshirish
+    if(!emailInput.includes("@gmail.com")) {
+        emailErrorSpan.textContent = "Iltimos, Gmail manzilini kiriting";
+    } else {
+        emailErrorSpan.textContent = "";
+    }
+
+    // parollar mos kelishini tekshirish
+    if (confirmPassword !== password || confirmPassword === "") {
         errorSpan.textContent = "Parollar mos kelmayapti";
     } else {
         errorSpan.textContent = "";
